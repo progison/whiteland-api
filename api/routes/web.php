34d1aa0b3cd345f95c7883d515d5fdf05be857fa
SwetaTsar <baltika5-7@yandex.ru>
2021-12-23 19:52:14 +0700

    configured laravel installation in subfolder /api

--- api/routes/web.php
+++ api/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('api')->group(
+    function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    }
+);
